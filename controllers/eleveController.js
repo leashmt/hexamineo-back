afa39f79af230bbbffce494464f49bc9188db3bc
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-12-18 09:42:25 +0100

    feat(eleve): add skip, repeat, and reset grade functionality with corresponding routes

--- controllers/eleveController.js
+++ controllers/eleveController.js
@@ -20,9 +20,7 @@ exports.getEleveById = async (req, res) => {
 };
 
 exports.createEleve = async (req, res) => {
-	console.log('posttttttt');
 	const { nom, prenom, dateDeNaissance, classe, niveau, prof } = req.body;
-	console.log('2');
 	const newEleve = new Eleve({
 		nom,
 		prenom,
@@ -32,12 +30,8 @@ exports.createEleve = async (req, res) => {
 		prof,
 	});
 
-	console.log(newEleve);
-
 	try {
 		const eleve = await newEleve.save();
-		console.log(eleve);
-		console.log('saved');
 		res.status(201).json(eleve);
 	} catch (error) {
 		res.status(400).json({ message: error.message });
@@ -61,6 +55,43 @@ exports.deleteEleve = async (req, res) => {
 		const eleve = await Eleve.findByIdAndDelete(req.params.id);
 		if (!eleve) return res.status(404).json({ message: 'Élève non trouvé' });
 		res.status(200).json({ message: 'Élève supprimé avec succès' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+exports.skipGrade = async (req, res) => {
+	try {
+		const eleve = await Eleve.findByIdAndUpdate(req.params.id, {
+			skipGrade: true,
+		});
+		if (!eleve) return res.status(404).json({ message: 'Élève non trouvé' });
+		res.status(200).json({ message: 'Élève saute une classe' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+exports.repeatGrade = async (req, res) => {
+	try {
+		const eleve = await Eleve.findByIdAndUpdate(req.params.id, {
+			repeatGrade: true,
+		});
+		if (!eleve) return res.status(404).json({ message: 'Élève non trouvé' });
+		res.status(200).json({ message: 'Élève redoublement une classe' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+exports.resetGrade = async (req, res) => {
+	try {
+		const eleve = await Eleve.findByIdAndUpdate(req.params.id, {
+			repeatGrade: false,
+			skipGrade: false,
+		});
+		if (!eleve) return res.status(404).json({ message: 'Élève non trouvé' });
+		res.status(200).json({ message: 'Élève réinitialisé' });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
