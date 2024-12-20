55073cdab0c5eab9541b767ecb5657bb0922a143
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-12-20 11:54:51 +0100

    refactor(server): improve code formatting and structure for route definitions

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const eleveRoutes = require('./routes/eleveRoutes');
@@ -9,7 +9,7 @@ const archiveRoutes = require('./routes/archiveRoutes');
 const csvRoutes = require('./routes/csvRoutes');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-const {authenticate, authorize} = require('./middleware/verifyToken');
+const { authenticate, authorize } = require('./middleware/verifyToken');
 
 const app = express();
 const PORT = 3001;
@@ -25,13 +25,28 @@ mongoose
 	.then(() => console.log('Connexion à MongoDB réussie'))
 	.catch(err => console.error('Erreur de connexion à MongoDB :', err));
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', authenticate, authorize(["ADMIN"]), userRoutes)
-app.use('/api/eleves', authenticate, authorize(["ADMIN", "MAIRIE", "DIRECTRICE", "PROFESSEUR"]), eleveRoutes);
-app.use('/api/professeurs', authenticate, authorize(['ADMIN', 'MAIRIE']), professeurRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', authenticate, authorize(['ADMIN']), userRoutes);
+app.use(
+	'/api/eleves',
+	authenticate,
+	authorize(['ADMIN', 'MAIRIE', 'DIRECTRICE', 'PROFESSEUR']),
+	eleveRoutes
+);
+app.use(
+	'/api/professeurs',
+	authenticate,
+	authorize(['ADMIN', 'MAIRIE', 'DIRECTRICE']),
+	professeurRoutes
+);
 app.use('/api/classes', authenticate, authorize(['ADMIN', 'DIRECTRICE']), classeRoutes);
-app.use('/api', authenticate, authorize(["ADMIN", "MAIRIE"]), csvRoutes);
-app.use('/api/archive', authenticate, authorize(["ADMIN", "MAIRIE"]), archiveRoutes);
+app.use('/api', authenticate, authorize(['ADMIN', 'MAIRIE']), csvRoutes);
+app.use(
+	'/api/archive',
+	authenticate,
+	authorize(['ADMIN', 'MAIRIE', 'DIRECTRICE']),
+	archiveRoutes
+);
 
 app.get('/', (req, res) => {
 	res.send('Bienvenue sur le serveur back-end Hexamineo !');
